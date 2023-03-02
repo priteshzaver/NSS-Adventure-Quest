fb99d9b778bb0b3d4e56d78154665f0066dde470
Pritesh Zaver <priteshzaver@gmail.com>
2023-03-02 09:37:52 -0600

    phase4

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ namespace Quest
         static void Main(string[] args)
         {
             Console.Write("Hello Adventurer! Please enter your name: ");
-
             // Create a few challenges for our Adventurer's quest
             // The "Challenge" Constructor takes three arguments
             //   the text of the challenge
@@ -45,8 +44,16 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer(Console.ReadLine());
-
+            Robe adventurerRobe = new Robe()
+            {
+                RobeColors = new List<string>()
+                {
+                    "red", "green", "yellow"
+                },
+                RobeLength = 42
+            };
+            Adventurer theAdventurer = new Adventurer(Console.ReadLine(), adventurerRobe);
+            Console.WriteLine($"{theAdventurer.GetDescription()}");
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
             List<Challenge> challenges = new List<Challenge>()
---
+++ Robe.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Quest
+{
+    public class Robe
+    {
+        public List<string> RobeColors {get; set;}
+        public int RobeLength { get; set; }
+    }
+}
